6ad282aa6cedf0ee9628e78e6930139b864481e0
Kai Yamada <gpioblink@aol.com>
2020-03-11 13:05:54 +0900

    compose the pages

---
+++ components/todo.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {compose} from 'recompose';
+
+const Todo = (props) => {
+    const {
+        item,
+        data,
+        onChangeTodo,
+        addTodo,
+        deleteTodo,
+    } = props;
+
+    return (
+        <React.Fragment>
+            <h1>TODO</h1>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                addTodo({
+                    value: item.value,
+                });
+            }}>
+                <input
+                  type="text"
+                  value={item.value}
+                  onChange={e => onChangeTodo({
+                      value: e.target.value,
+                  })}
+                />
+                <br />
+            </form>
+            <hr />
+            {data.map((item, index) => (
+                <p key={index}>
+                    {item.value}
+                    {' '}
+                    <button onClick={() => deleteTodo(item)}>
+                        DELETE
+                    </button>
+                </p>
+            ))}
+        </React.Fragment>
+    );
+};
+
+export default compose()(Todo);
