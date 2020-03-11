13668d67efef89f4ff290ddf6bdfe6ed4ba67655
Kai Yamada <gpioblink@aol.com>
2020-03-11 13:36:36 +0900

    rewrite page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,30 @@
-export default () => <p>Hello World</p>
+import {
+    NextPageContext,
+    NextComponentType,
+} from 'next';
+import {compose} from 'recompose';
+import {connect} from 'react-redux';
+import Page from '../containers/page';
+import {addTodo} from '../actions';
+import {Store} from '../store';
+
+interface IndexPageContext extends NextPageContext {
+    store: Store;
+};
+
+const IndexPage: NextComponentType<IndexPageContext> = compose()(Page);
+
+IndexPage.getInitialProps = ({store, req}) => {
+    const isServer: boolean = !!req;
+
+    const {todo} = store.getState();
+    store.dispatch(addTodo(Object.assign(todo.item, {
+        value: 'Hello world!',
+    })));
+
+    return {
+        isServer,
+    };
+};
+
+export default connect()(IndexPage);
